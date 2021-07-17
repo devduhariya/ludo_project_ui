af88e9d2895299725b2da0583300e7d3430d41d4
devduhariya <64675676+devduhariya@users.noreply.github.com>
2021-07-17 18:45:48 +0530

    error

--- src/Components/Totalchips.js
+++ src/Components/Totalchips.js
@@ -9,8 +9,6 @@ const Totalchips = () => {
     };
 
     const [data, setdata] = useState([]);
-
-z
     const getchips = () => {
         Axios.get('https://ludo-project-backend.herokuapp.com/api/buyChips/totalchips', config).then(res => {
             // console.log('res: ', res);
