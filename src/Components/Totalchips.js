ca9bf5a7458db53c1ce768d319e34c9b87978d2d
AarushiGaur <aarushigaur91@gmail.com>
2021-07-20 11:58:31 +0530

    commit

--- src/Components/Totalchips.js
+++ src/Components/Totalchips.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react'
 import Axios from 'axios';
-//import Header from './Header';
 
 const Totalchips = () => {
     var token = JSON.parse(localStorage.getItem('login'));
@@ -13,9 +12,7 @@ const Totalchips = () => {
 
     const getchips = () => {
         Axios.get('https://ludo-project-backend.herokuapp.com/api/buyChips/totalchips', config).then(res => {
-            //console.log('res: ', res.data);
             setdata(res.data)
-            //console.log(res)
         }).catch(error => {
             console.log('Error: ', error);
         });
@@ -24,10 +21,7 @@ const Totalchips = () => {
 
 
     useEffect(() => {
-        // Axios.get( 'http://localhost:9000/api/buyChips',config).then(res=>{setdata({...data,data:res.data})}).catch(console.error());
-        // setTimeout(() => {
         getchips()
-        // }, 3000);
     }, [])
 
     return (
