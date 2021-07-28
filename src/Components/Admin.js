ff73796108bf119b84249704ea3f6c621f59126f
AarushiGaur <aarushigaur91@gmail.com>
2021-07-28 13:04:14 +0530

    admin

--- src/Components/Admin.js
+++ src/Components/Admin.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Admin = () => {
     return (
         <div>
-            <a href="/AdminBuyChips">Admin Buy Chips</a><br/>
-            <a href="/AdminSellChips">Admin Sell Chips</a><br/>
+            <a href="/AdminBuyChips">Buy Chips Request</a><br/>
+            <a href="/AdminSellChips">Sell Chips Request</a><br/>
             <a href="/Admin/EditPlayerAmount">Edit Player Amount</a>
         </div>
     )
